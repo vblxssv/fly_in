9792c5198a787db249ce657fe45f6ea106386a38
vledbress <vledbress388@gmail.com>
2026-07-18 20:34:08 +0200

    .

--- src/renderer/renderer.py
+++ src/renderer/renderer.py
@@ -1,20 +1,21 @@
 from abc import ABC, abstractmethod
-from src.models.move import Move
-from src.models.state import SimulationState
+from src.models.frame import Frame
 from typing import List
 
 
 class IRenderer(ABC):
     @abstractmethod
-    def render(self, state: SimulationState, moves: List[Move]) -> None:
+    def render(self, frames: List[Frame]) -> None:
         pass
 
 
 class ConsoleRenderer(IRenderer):
-    def render(self, state: SimulationState, moves: List[Move]) -> None:
+    @abstractmethod
+    def render(self, frames: List[Frame]) -> None:
         pass
 
 
 class PyGameRenderer(IRenderer):
-    def render(self, state: SimulationState, moves: List[Move]) -> None:
+    @abstractmethod
+    def render(self, frames: List[Frame]) -> None:
         pass
